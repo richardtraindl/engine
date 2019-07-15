dcefd35f91a58775fb8249d6ba63345d16ded2f9
Richard Traindl <richardtraindl@gmail.com>
2019-07-15 14:04:21 +0200

    Update blackpawn.hpp

--- pieces/blackpawn.hpp
+++ pieces/blackpawn.hpp
@@ -10,15 +10,15 @@
         public:
             static int STEPS[2];
             static int MAXCNT;
-            static vector<pair<int, unsigned>> MV_STEPS_RANK7;
-            static vector<pair<int, unsigned>> MV_STEPS_RANK2;
-            static vector<pair<int, unsigned>> MV_STEPS;
-            
-            cBlackPawn(cBoard *board, unsigned pos);
-            static int dir_for_move(unsigned src, unsigned dst);
-            static int step_for_dir(unsigned dir);
-            bool is_move_valid(unsigned dst, unsigned prompiece, list<cMove> *minutes);
-            bool is_ep_move_ok(unsigned dst, list<cMove> *minutes);
+            static int MV_STEPS_RANK7[4][2];
+            static int MV_STEPS_RANK2[12][2];
+            static int MV_STEPS[3][2];
+
+            cBlackPawn(cBoard *board, int pos);
+            static int dir_for_move(int src, int dst);
+            static int step_for_dir(int dir);
+            bool is_move_valid(int dst, int prompiece, list<cMove> *minutes);
+            bool is_ep_move_ok(int dst, list<cMove> *minutes);
     };
 
 #endif
