e36291e739392efced43bb1335befe5369cc7c99
Richard Traindl <richardtraindl@gmail.com>
2019-07-15 13:51:39 +0200

    Update queen.cpp

--- pieces/queen.cpp
+++ pieces/queen.cpp
@@ -8,14 +8,13 @@
     cQueen::cQueen(cBoard *board, int pos) : cPiece(board, pos), cRook(board, pos), cBishop(board, pos){
     }
 
-    unsigned cQueen::DIRS_ARY[8] = 
+    int cQueen::DIRS_ARY[8] = 
         {DIRS["nth"],     DIRS["sth"],     DIRS["est"],     DIRS["wst"],
          DIRS["nth-est"], DIRS["sth-wst"], DIRS["nth-wst"], DIRS["sth-est"]};
     int cQueen::STEPS[8] = 
         {8, -8, 1, -1, 9, -9, 7, -7};
-    vector<pair<int, unsigned>> cQueen::MV_STEPS = 
-        {make_pair(8, PIECES["blk"]), make_pair(-8, PIECES["blk"]), make_pair(1, PIECES["blk"]), make_pair(-1, PIECES["blk"]), 
-         make_pair(9, PIECES["blk"]), make_pair(-9, PIECES["blk"]), make_pair(7, PIECES["blk"]), make_pair(-7, PIECES["blk"])};
+    int cQueen::MV_STEPS[8][2] = {{8, PIECES["blk"]}, {-8, PIECES["blk"]}, {1, PIECES["blk"]}, {-1, PIECES["blk"]}, 
+                                  {9, PIECES["blk"]}, {-9, PIECES["blk"]}, {7, PIECES["blk"]}, {-7, PIECES["blk"]}};
 
     int cQueen::dir_for_move(int src, int dst){
         int dir = cRook::dir_for_move(src, dst);
