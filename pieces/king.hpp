d1d3a351164f63f8eb676d2d643b8f3ec4cb7305
Richard Traindl <richardtraindl@gmail.com>
2019-07-15 13:58:23 +0200

    Update king.hpp

--- pieces/king.hpp
+++ pieces/king.hpp
@@ -9,18 +9,18 @@
 
     class cKing : public cPiece{
         public:
-            static unsigned DIRS_ARY[1];
+            static int DIRS_ARY[1];
             static int STEPS[8];
-            static vector<pair<int, unsigned>> MV_STEPS;
+            static int MV_STEPS[8][2];
             static int MAXCNT;
 
-            cKing(cBoard *board, unsigned pos);
+            cKing(cBoard *board, int pos);
             bool is_trapped();
-            bool is_move_stuck(unsigned dst);
-            bool is_move_valid(unsigned dst, unsigned prompiece);
-            bool is_short_castling_ok(unsigned dst);
-            bool is_long_castling_ok(unsigned dst);
-            cMove *do_move(unsigned dst, unsigned prompiece, int movecnt, int *score);
+            bool is_move_stuck(int dst);
+            bool is_move_valid(int dst, int prompiece);
+            bool is_short_castling_ok(int dst);
+            bool is_long_castling_ok(int dst);
+            cMove *do_move(int dst, int prompiece, int movecnt, int *score);
             bool undo_move(cMove *move, int movecnt, int *score);
             int score_touches();
             // bool is_safe();
