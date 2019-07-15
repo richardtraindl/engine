b3b34c57ab0c05a9139d45f2305c025c97fbe2b1
Richard Traindl <richardtraindl@gmail.com>
2019-07-15 13:45:54 +0200

    Update rook.hpp

--- pieces/rook.hpp
+++ pieces/rook.hpp
@@ -8,14 +8,14 @@
 
     class cRook : virtual public cPiece{
         public:
-            static unsigned DIRS_ARY[4];
+            static int DIRS_ARY[4];
             static int STEPS[4];
-            static vector<pair<int, unsigned>> MV_STEPS;
+            static int MV_STEPS[4][2];
 
             cRook(cBoard *board, unsigned pos);
-            static int dir_for_move(unsigned src, unsigned dst);
-            static int step_for_dir(unsigned dir);
-            cMove *do_move(unsigned dst, unsigned prompiece, int movecnt, int *score);
+            static int dir_for_move(int src, int dst);
+            static int step_for_dir(int dir);
+            cMove *do_move(int dst, int prompiece, int movecnt, int *score);
             bool undo_move(cMove *move, int movecnt, int *score);
     };
 
