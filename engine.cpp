086d354ebea2b98958c51b7c69b3acead4309c41
Richard Traindl <richardtraindl@gmail.com>
2019-07-16 13:12:11 +0200

    Update engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -20,13 +20,13 @@
 
         cout << "is_fifty_moves_rule() " << match.is_fifty_moves_rule() << "\n";
         
-        match.board.setfield(8, PIECES["blk"]);
-        cMove *move = match.do_move(0, 16, PIECES["blk"]);
+        match.board.setfield(8, mBLK);
+        cMove *move = match.do_move(0, 16, mBLK);
         cout << "do_move " << hex << match.board.fields << "\n";
         cout << "move " << hex << move->format() << "\n";
 
-        //move = match.do_move(52, 44, PIECES["blk"]);
-        move = match.do_move(9, 17, PIECES["blk"]);
+        //move = match.do_move(52, 44, mBLK);
+        move = match.do_move(9, 17, mBLK;
         cout << "do_move " << hex << match.board.fields << "\n";
         cout << "move " << hex << move->format() << "\n";
 
@@ -35,7 +35,7 @@
 
         cout << "is_king_after_move_attacked() " << match.board.is_king_after_move_attacked(11, 12, &match.minutes) << "\n";
 
-        cout << "is_move_valid() " << match.board.is_move_valid(0, 9,PIECES["blk"], &match.minutes) << "\n";
+        cout << "is_move_valid() " << match.board.is_move_valid(0, 9, mBLK, &match.minutes) << "\n";
 
         cout << "is_move_available() " << match.board.is_move_available(&match.minutes) << "\n";
         
@@ -45,16 +45,5 @@
 
         cout << "eval_soft_pin_dir() " << match.board.eval_soft_pin_dir(42) << "\n";
         
-        
-        // cout << "match.board->fields " << match.board.getFields();
-        
-        // struct session kant;
-        // neu(&kant);
-        // init_words();
-        // forth(&kant);
-
-        // perform_respond(&kant);
-
         return 0;
     }
-
