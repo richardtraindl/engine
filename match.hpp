4fba11f63813d7af79f89984adda6f5ea8bad1c8
Richard Traindl <richardtraindl@gmail.com>
2019-07-22 10:46:32 +0200

    Update match.hpp

--- match.hpp
+++ match.hpp
@@ -13,6 +13,11 @@
 
     class cMatch{
         public:
+            static map<string, int> STATUS;
+            static map<string, int> LEVELS;
+            static map<int, int> SECS_PER_MOVE;
+            static map<string, int> RETURN_CODES;
+
             time_t created_at;
             int status;
             int score;
@@ -20,22 +25,28 @@
             cBoard board;
             list<cMove> minutes;
 
-            static map<string, int> STATUS;
-            static map<string, int> LEVELS;
-            static map<int, int> SECS_PER_MOVE;
-            static map<string, int> RETURN_CODES;
-
             cMatch();
+
             cMatch(const cMatch &obj);
+
             void update_attributes();
+
             int movecnt();
+
             int next_color();
+
             bool is_opening();
+
             bool is_endgame();
+
             bool is_fifty_moves_rule();
+
             bool is_move_repetition();
+
             cMove *do_move(int src, int dst, int prompiece);
+
             bool undo_move();
+
             int eval_status();
     };
 
