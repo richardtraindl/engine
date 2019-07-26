7b174d96a04bb62b85c7d7c8b6c552765485d1df
Richard Traindl <richardtraindl@gmail.com>
2019-07-26 16:47:49 +0200

    Update analyze_position.hpp

--- compute/analyze_position.hpp
+++ compute/analyze_position.hpp
@@ -5,32 +5,32 @@
 
     using namespace std;
 
-    int score_traps_and_touches(cMatch *match);
+    int score_traps_and_touches(cMatch &match);
 
-    //int score_controled_horizontal_files(cMatch *match);
+    //int score_controled_horizontal_files(cMatch &match);
 
-    //int score_controled_vertical_files(cMatch *match);
+    //int score_controled_vertical_files(cMatch &match);
 
-    int score_kings_safety(cMatch *match);
+    int score_kings_safety(cMatch &match);
 
-    int score_penalty_for_lost_castlings(cMatch *match);
+    int score_penalty_for_lost_castlings(cMatch &match);
 
-    int score_penalty_for_multiple_moves(cMatch *match);
+    int score_penalty_for_multiple_moves(cMatch &match);
 
-    int score_penalty_for_knight_bishop_on_baseline(cMatch *match);
+    int score_penalty_for_knight_bishop_on_baseline(cMatch &match);
 
-    int score_weak_pawns(cMatch *match);
+    int score_weak_pawns(cMatch &match);
 
-    int score_penalty_for_weak_fianchetto(cMatch *match);
+    int score_penalty_for_weak_fianchetto(cMatch &match);
 
-    int score_opening(cMatch *match);
+    int score_opening(cMatch &match);
 
-    int score_middlegame(cMatch *match);
+    int score_middlegame(cMatch &match);
 
-    int score_endgame(cMatch *match);
+    int score_endgame(cMatch &match);
 
-    int score_position(cMatch *match, int movecnt);
+    int score_position(cMatch &match, int movecnt);
 
-    bool is_stormy(cMatch *match);
+    bool is_stormy(cMatch &match);
 
 #endif
