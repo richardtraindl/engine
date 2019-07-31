61fa6bbdd9194e932ce2e9df4fc7e403b946797c
Richard Traindl <richardtraindl@gmail.com>
2019-07-31 17:22:16 +0200

    Update brick.hpp

--- bricks/brick.hpp
+++ bricks/brick.hpp
@@ -1,4 +1,7 @@
 	
+#ifndef BRICK_HPP
+    #define BRICK_HPP    
+
     #include <boost/multiprecision/cpp_int.hpp>
 
     using namespace boost::multiprecision;
@@ -21,24 +24,4 @@
 			      bool tst_blanks(uint256_t fields);
     };
 
-	cBrick::cBrick(uint256_t newshape){
-		shape = newshape;
-		bits1000 = shape & BITS1000;
-		tst_blank;
-	}
-
-
-	bool cBrick::tst_whites(uint256_t fields){
-		uint256_t cuts = fields & shape;
-		return cuts > 0 && (cuts & bits1000) == 0;
-	}
-
-	bool cBrick::tst_blacks(uint256_t fields){
-		uint256_t cuts = fields & shape;
-		return (cuts & bits1000) > 0;
-	}
-
-	bool cBrick::tst_blanks(uint256_t fields){
-		uint256_t cuts = fields & shape;
-		return cuts == 0;
-	}
+#endif
