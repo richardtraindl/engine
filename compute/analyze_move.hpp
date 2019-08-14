8de839644073569a48b1a76e8767f76d930dc5d0
Richard Traindl <richardtraindl@gmail.com>
2019-08-14 10:15:49 +0200

    Update analyze_move.hpp

--- compute/analyze_move.hpp
+++ compute/analyze_move.hpp
@@ -4,9 +4,12 @@
     #include <list>
     #include "../match.hpp"
     #include "../move.hpp"
+    #include "../pieces/piece.hpp"
 
     using namespace std;
 
     void add_tactics(cPrioMove &priomove, cMatch &match, cMove *candidate, cMove *dbggmove, list<cExclude> &excludes);
+
+    bool find_excluded(list<cExclude> excludes, int pos, int touch_pos, int tactic_domain);
 
 #endif
