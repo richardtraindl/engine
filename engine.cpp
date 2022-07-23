18752a5123e4aaa898a200544738d54e964accf8
Richard Traindl <richardtraindl@gmail.com>
2022-07-23 22:44:38 +0200

    Update engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -5,223 +5,40 @@
     #include <string>
     #include <algorithm>
     #include "./match.hpp"
-    #include "./move.hpp"
-    #include "./endgame.hpp"
-    #include "./test.hpp"
+    #include "./util/test.hpp"
 
 
     using namespace std;
 
 
-    bool import_minutes(cMatch &match, string path_and_filename){
+  void prnt_minutes(const cMatch &match){
 
-        match.reset();
+      for(const cMove &move : match.m_minutes){
 
-        ifstream file(path_and_filename);
-        if(file.fail()){
-            cout << "file error!" << endl;
-            return false;
-        }
+          cout << move.format(false) << endl;
 
-        string content;
+      }
 
-        uint8_t src_x, src_y, dst_x, dst_y;
-
-        uint8_t prompiece = mBLK;
-
-        while(file >> content) {
-
-            cMove::coord_to_indices(src_x, src_y, content.substr(0, 2));
-
-            cMove::coord_to_indices(dst_x, dst_y, content.substr(2, 2));
-            
-            if(match.is_move_valid(src_x, src_y, dst_x, dst_y, prompiece)){
-                match.do_usr_move(src_x, src_y, dst_x, dst_y, prompiece);
-            }
-            else{
-                cout << "import error!" << endl;
-                return false;
-            }
-        }
-
-        return true;
-
-    }
+  }
 
 
-    bool import_board(cMatch &match, string path_and_filename){
-
-        match.reset();
-
-        ifstream file(path_and_filename);
-        if(file.fail()){
-            cout << "file error!" << endl;
-            return false;
-        }
-        
-        string content;
-        bool white;
-        bool opening;
-        
-        file >> content;
-        if(content.substr(0, 5) == "white"){
-            white = true;
-        }
-        else{
-            white = false;
-        }
-
-        file >> content;
-        if(content == "opening"){
-            opening = true;
-        }
-        else{
-            opening = false;
-        }
-
-        uint8_t y = 7;
-        int32_t sum = 0;
-        while(file >> content) {
-
-            if(content.length() == 32){
-
-                for(uint8_t x = 0; x < 8; ++x){
-
-                    uint8_t piece = PIECES[content.substr((x * 4), 3)];
-
-                    match.m_board.setfield(x, y, piece);
-
-                }
-
-                --y;
-
-            }
-            else{
-                cout << "import error!" << endl;
-                return false;
-            }
-        }
-
-        // check kings and set vars
-        bool wkg = false;
-        bool bkg = false;
-
-        for(uint8_t y = 0; y < 8; ++y){
-
-            for(uint8_t x = 0; x < 8; ++x){
-
-                uint8_t piece = match.m_board.getfield(x, y);
-
-                if(piece == mWKG){
-                    if(wkg){ 
-                        cout << "import error: wkg!" << endl;
-                        return false;
-                    }
-                    else{
-                        wkg = true;
-                        match.m_board.m_wKg_x = x;
-                        match.m_board.m_wKg_y = y;
-                    }
-                }
-                else if(piece == mBKG){
-                    if(bkg){ 
-                        cout << "import error: bkg!" << endl;
-                        return false;
-                    }
-                    else{
-                        bkg = true;
-                        match.m_board.m_bKg_x = x;
-                        match.m_board.m_bKg_y = y;
-                    }
-                }
-                else{
-                    sum += SCORES[piece];
-                }
-            }
-        }
-                    
-        if(wkg && bkg){
-            match.m_board.m_wKg_has_moved_at = 1;
-            match.m_board.m_bKg_has_moved_at = 1;
-            match.m_board.m_wRkA_has_moved_at = 1;
-            match.m_board.m_wRkH_has_moved_at = 1;
-            match.m_board.m_bRkA_has_moved_at = 1;
-            match.m_board.m_bRkH_has_moved_at = 1;
-            
-            match.m_score = sum * -1;
-
-            //perform fake moves to pretend not to be opening
-            uint8_t maxidx;
-            if(opening){
-                white ? maxidx = 20 : maxidx = 19;
-            }
-            else{
-                white ? maxidx = 32 : maxidx = 31;
-            }
-
-            for(uint8_t i = 0; i < maxidx; ++i){
-                match.m_minutes.push_back(cMove(1, 1, 1, 1, 1, 1, 1, 100));
-            }
-
-            return true;
-        }
-        else{
-            cout << "import error: kg!" << endl;
-            return false;
-        }
-
-    }
+  void prnt_status(const cMatch &match, const uint8_t status){
+      if(status == cMatch::STATUS_WINNER_WHITE){
+          prnt_minutes(match);
+          cout << "winner white!" << endl;
+      }
+      else if(status == cMatch::STATUS_WINNER_BLACK){
+          prnt_minutes(match);
+          cout << "winner black!" << endl;
+      }
+      else if(status == cMatch::STATUS_DRAW){
+          prnt_minutes(match);
+          cout << "draw!" << endl;
+      }
+  }
 
 
-    bool import(cMatch &match, string filename){
-
-        string path_and_filename = "./matches/" + filename + ".txt";
-        ifstream file(path_and_filename);
-        if(file.fail()){
-            cout << "file error!" << endl;
-            return false;
-        }
-
-        string content;
-
-        uint8_t identify_file = 0;
-
-        while(file >> content) {
-
-            if(content.length() == 0){
-                continue;
-            }
-
-            if(content.length() > 0 && content.length() < 6){
-                if(content.substr(0, 5) == "white" || content.substr(0, 5) == "black"){
-                    identify_file = 2;
-                    break;
-                }
-                else{
-                    identify_file = 1;
-                    break;
-                }
-            }
-
-            if(content.length() == 32){
-                identify_file = 2;
-                break;
-            }
-        }
-
-        if(identify_file == 1){
-            return import_minutes(match, path_and_filename);
-        }
-        else if(identify_file == 2){
-            return import_board(match, path_and_filename);
-        }
-
-        return false;
-
-    }
-
-
-    void play(cMatch &match, uint8_t engine_color){
+  void play(cMatch &match, uint8_t engine_color){
 
         char buffer[31];
         int length = 32;
@@ -247,14 +64,22 @@
 
                 if(rmoves.size() > 0){
                    cMove move = rmoves.front();
+
                    match.do_move(move);
+
                    cout << move.format(false) << endl;
+
+                   cout << "m_score: " << match.m_score << endl;
+
                    match.m_board.prnt();
                 }
                 else{
                     uint8_t status = match.eval_status();
-                    match.prnt_status(status);
+
+                    cout << "status " << status;
+
                     cout << "game over - congratulation!" << endl;
+
                     return;
                 }
             }
@@ -262,7 +87,7 @@
                 uint8_t status = match.eval_status();
 
                 if(status != cMatch::STATUS_OPEN){
-                    match.prnt_status(status);
+                    cout << "status " << status;
                 }
 
                 cout << "\n>";
@@ -272,12 +97,13 @@
 
                 if(input == "h" || input == "H"){ 
                     cout << "\nmove formats: e2e4 | e7e8q | 0-0 | 0-0-0";
-                    cout << "\ncommands: quit) | u(ndo) | w(hite engine) | b(lack engine) | pb(print board) | pm(print minutes) | im(import file)";
+                    cout << "\ncommands: u(ndo) | w(hite engine) | b(lack engine) | pb(print board) | pm(print minutes)";
                     continue;
                 }
 
                 if(input == "quit" || input == "QUIT"){ 
                     cout << "good bye!" << endl; 
+
                     return; 
                 }
 
@@ -290,84 +116,30 @@
                         match.undo_move(); // undo last human move
                     }
                     match.m_board.prnt();
+
                     continue;
                 }
 
                 if(input == "pb" || input == "PB"){
                     match.m_board.prnt();
+
                     continue;
                 }
 
                 if(input == "pm" || input == "PM"){
-                    match.prnt_minutes();
-                    continue;
-                }
-
-                if(input.substr(0, 2) == "im" || input.substr(0, 2) == "IM"){
-                    if(input.length() < 5){
-                        cout << "format for import: 'im <filename>'" << endl;
-                        continue;
-                    }
-
-                    if(import(match, input.substr(2))){
-                        //match.prnt_minutes();
-
-                        match.m_board.prnt();
-                        cout << "score: " << to_string(match.m_score) << endl;
-                    }
-                    else{
-                          match.reset();
-                    }
+                    prnt_minutes(match);
 
                     continue;
-
                 }
 
                 if(input == "w" || input == "W"){
                     engine_color = mWHITE;
+
                     continue;
                 }
 
                 if(input == "b" || input == "B"){
                     engine_color = mBLACK;
-                    continue;
-                }
-
-                if(input.compare("test") == 0){
-                    cout << "test start" << endl;
-
-                    //prnt_moves_weak_state(match);
-                    prnt_eval_field_states(match);
-
-                    //prnt_moves_score(match);
-
-                    //cMove move(3, 3, 3, 2, mBPW, mBLK, mBLK, 0);
-                    //prnt_does_move_touch_weak_piece(match, move);
-
-                    //vector<cMove> moves;
-                    //match.gen_moves(moves, match.next_color());
-                    //sort(moves.begin(), moves.end(), match.sortByPrio);
-                    //for(const cMove &move : moves){
-                    //    cout << move.format(true) << endl;
-                    //}
-
-                    //prnt_test(match);
-
-                    //prnt_moves_touch_weak_piece(match);
-
-                    //cPiece piece(mWPW, 0, 3);
-                    //prnt_eval_pin_state(match, piece);
-
-                    //cMove move(2, 0, 0, 0, mWRK, mBLK, mBLK, 0);
-                    //prnt_does_move_touch_weak_piece(match, move);
-
-                    //prnt_moves_weak_state(match);
-
-                    //play100(match, engine_color);
-
-                    //prnt_eval_field_states(match);
-
-                    //prnt_moves_score(match);
 
                     continue;
                 }
@@ -424,7 +196,7 @@
 
                 if(match.is_move_valid(src_x, src_y, dst_x, dst_y, prompiece)){
 
-                    match.do_usr_move(src_x, src_y, dst_x, dst_y, prompiece);
+                    do_usr_move(match, src_x, src_y, dst_x, dst_y, prompiece);
 
                     match.m_board.prnt();
                 }
@@ -437,11 +209,58 @@
 
 
     int main(void){
+
         cMatch match;
 
-        uint8_t engine_color = mBLANK;
+        uint8_t engine_color = mWHITE;
+
+        import_minutes(match, "morphy");
+        
+        if(match.m_board.getfield(match.m_board.m_wKg_x, match.m_board.m_wKg_y) != mWKG || match.m_board.getfield(match.m_board.m_bKg_x, match.m_board.m_bKg_y) != mBKG){
+            cout << "DAMAGE" << endl;
+            return 0;
+        }
+        else{
+            cout << "OK" << endl;
+        }
+
+        match.m_board.prnt();
+        
+        //bool touched = match.m_board.is_field_touched(6, 6, mWHITE);
+        //cout << touched << endl;
+        
+        //vector<cPiece> wpieces, bpieces;
+        //match.m_board.search_all_dirs_for_touching_pieces(wpieces, bpieces, 6, 6, match.get_last_move(), false);
+        //cout << to_string(wpieces.size()) << " " << to_string(bpieces.size()) << endl;
+
+        //bool is_softpin = match.m_board.is_soft_pinned(5, 5);
+        //cout << "is_softpin: " << is_softpin << endl;
+        
+        //bool valid = match.is_move_valid(4, 7, 3, 7, mBLK);
+        //cout << "valid: " << valid << endl;
+
+        //test_eval_piece_state(match);
+      
+        //test_search_all_dirs_for_touching_pieces(match);
+
+        //cMove move(4, 0, 2, 0, mWKG, mBLK, mBLK, 0);
+        //cMove move2(4, 0, 6, 0, mWKG, mBLK, mBLK, 0);
+        
+        //int32_t score = cEvaluator::score_move(match.m_board, move);
+        //cout << "score: " << score << endl;
+        
+        //int32_t score2 = cEvaluator::score_board(match.m_board, 1, 1);
+        //cout << "score2: " << score2 << endl;
+
+        //test_eval_field_state(match, 3, 6);
+
+        //test_eval_piece_state(match);
+        
+        //test_is_hotspot(match);
 
         play(match, engine_color);
 
         return 0;
+
     }
+
