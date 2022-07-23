26401edf54ff0fa2f5bdda2642a8c2af682c692e
Richard Traindl <richardtraindl@gmail.com>
2022-07-23 22:54:00 +0200

    Update piece.hpp

--- piece.hpp
+++ piece.hpp
@@ -1,30 +1,69 @@
 
-#ifndef PIECE_HPP
-    #define PIECE_HPP
-    
-    #include <cstdint>
+
+    #ifndef PIECE_HPP
+
+      #define PIECE_HPP
 
 
-    using namespace std;
+      #include <cstdint>
+      #include <array>
+      #include "./values.hpp"
 
 
-    class cPiece{
+      using namespace std;
+
+
+      class cDir{
+
         public:
-            uint8_t m_piece;
-            uint8_t m_xpos;
-            uint8_t m_ypos;
+          uint16_t m_dir;
 
-            cPiece(uint8_t piece, uint8_t xpos, uint8_t ypos);
+          uint16_t m_cardinale;
 
-    };
+          int8_t m_xstep;
+
+          int8_t m_ystep;
+
+          cDir(uint16_t dir, uint16_t cardinale, int8_t xstep, int8_t ystep);
+
+      };
 
 
-    class cTouchedPiece : public cPiece{
+      class cPiece{
+
         public:
-            uint8_t m_dir;
-        
-            cTouchedPiece(uint8_t piece, uint8_t xpos, uint8_t ypos, uint8_t dir);
+          uint8_t m_piece;
 
-    };
+          uint8_t m_xpos;
 
-#endif
+          uint8_t m_ypos;
+
+          cPiece(uint8_t piece, uint8_t xpos, uint8_t ypos);
+
+          cPiece(const cPiece &piece);
+
+          static cDir all_steps[20];
+
+          static cDir rk_steps[4];
+
+          static cDir bp_steps[4];
+
+          static cDir qu_steps[8];
+
+          static cDir kn_steps[8];
+
+          static cDir wpw_steps[2];
+
+          static cDir wpw_capture_steps[2];
+
+          static cDir bpw_steps[2];
+
+          static cDir bpw_capture_steps[2];
+          
+          static void get_range(uint8_t range[3], uint8_t piece);
+
+          static bool is_touching_dir(uint8_t piece, uint16_t dir, uint8_t src_x, uint8_t src_y, uint8_t dst_x, uint8_t dst_y);
+
+      };
+
+    #endif
