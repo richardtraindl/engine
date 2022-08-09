778b07e5fe1363493ea25b1320ea02083f214c84
richardtraindl <richardtraindl@gmail.com>
2022-08-09 17:06:20 +0200

    huge changes

--- util/test.cpp
+++ util/test.cpp
@@ -4,8 +4,12 @@
     #include <iostream>
     #include <fstream>
     #include <string>
+    #include <vector>
     #include "../evaluator.hpp"
+    #include "../generator.hpp"
+    #include "../generator2.hpp"
     #include "../match.hpp"
+    #include "../daemon.hpp"
     #include "../move.hpp"
     #include "./test.hpp"
 
@@ -93,25 +97,6 @@
   }
 
 
-  void test_is_hotspot(cMatch &match){
-
-      for(int8_t y = 7; y >= 0; --y){
-
-          for(uint8_t x = 0; x <= 7; ++x){
-            
-              bool flag = cEvaluator::is_hotspot(match, x, y);
-
-              cout << setw(5) << setfill(' ') <<  to_string(flag);
-
-          }
-          cout << endl;
-          cout << endl;
-
-      }
-
-  }
-
-
   void test_eval_field_state(cMatch &match, const uint8_t src_x, const uint8_t src_y){
 
       uint8_t field_state = cEvaluator::eval_field_state(match, src_x, src_y);
@@ -133,4 +118,184 @@
     }
 
   }
+
+
+  void test_gen2(cMatch &match){
+
+    cGenerator2 gen2(match);
+
+    cMove *moveptr;
+
+    while(true){
+
+        moveptr = gen2.gen_move();
+
+        if(moveptr != nullptr){
+            cout << moveptr->format(true) << endl;
+
+            delete moveptr;
+        }
+        else{
+            return;
+        }
+
+    }
+
+  }
+
+
+  void test_does_move_escape_soft_pin(cMatch &match, const cMove &move){
+
+      bool flag = match.m_board.is_piece_behind_soft_pinned(4, 6);
+      cout << flag << endl;
+
+      flag = cEvaluator::does_move_escape_soft_pin(match, move);
+      
+      cout << flag << endl;
+
+  }
+
+
+  void test_is_continue(cMatch &match, const uint8_t depth){
+
+      vector<cMove> moves;
+
+      cGenerator gen(match);
+      gen.gen_moves(moves, match.next_color());
+
+      //match.sort(moves.begin(), moves.end(), match.sortByPrio);
+
+      cDaemon daemon(match);
+
+      for(const cMove &move : moves){
+
+          cout << "started for: " + move.format(true) << endl;
+
+          bool flag = daemon.is_continue(match, move, depth, 1);
+
+          cout << flag << endl;
+
+          cout << "************" << endl;
+
+        }
+
+  }
+
+
+  void test_is_continue_sequence(cMatch &match){
+
+        uint8_t src_x, src_y, dst_x, dst_y, depth;
+
+        vector<cMove> sequence;
+        
+        cMove *moveptr;
+
+        cMove::coord_to_indices(src_x, src_y, "h1");
+        cMove::coord_to_indices(dst_x, dst_y, "d1");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mWRK, mBLK, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "e7");
+        cMove::coord_to_indices(dst_x, dst_y, "e6");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mBQU, mBLK, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "b5");
+        cMove::coord_to_indices(dst_x, dst_y, "d7");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mWBP, mBRK, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "f6");
+        cMove::coord_to_indices(dst_x, dst_y, "d7");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mBKN, mWBP, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "b3");
+        cMove::coord_to_indices(dst_x, dst_y, "b8");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mWQU, mBLK, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "d7");
+        cMove::coord_to_indices(dst_x, dst_y, "b8");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mBKN, mWQU, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        cMove::coord_to_indices(src_x, src_y, "d1");
+        cMove::coord_to_indices(dst_x, dst_y, "d8");
+        moveptr = new cMove(src_x, src_y, dst_x, dst_y, mWRK, mBLK, mBLK, cMove::P_MEDIUM);
+        cEvaluator::priorize_move(match, *moveptr);
+        sequence.push_back(*moveptr);
+
+        match.do_move(*moveptr);
+
+        /*cMove::coord_to_indices(src_x, src_y, "f6");
+        cMove::coord_to_indices(dst_x, dst_y, "d7");
+        sequence.push_back(cMove(src_x, src_y, dst_x, dst_y, mBKN, mWBP, mBLK, cMove::P_MEDIUM));
+
+        cMove::coord_to_indices(src_x, src_y, "b3");
+        cMove::coord_to_indices(dst_x, dst_y, "b8");
+        sequence.push_back(cMove(src_x, src_y, dst_x, dst_y, mWQU, mBLK, mBLK, cMove::P_MEDIUM));
+
+        cMove::coord_to_indices(src_x, src_y, "d7");
+        cMove::coord_to_indices(dst_x, dst_y, "b8");
+        sequence.push_back(cMove(src_x, src_y, dst_x, dst_y, mBKN, mWQU, mBLK, cMove::P_MEDIUM));
+
+        cMove::coord_to_indices(src_x, src_y, "d1");
+        cMove::coord_to_indices(dst_x, dst_y, "d8"); 
+        sequence.push_back(cMove(src_x, src_y, dst_x, dst_y, mWRK, mBLK, mBLK, cMove::P_MEDIUM));*/
+
+        depth = 1;
+
+        cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" << endl;
+
+        for(const cMove &move : sequence){
+
+            cDaemon daemon(match);
+
+            bool flag = daemon.is_continue(match, move, depth, 1);
+
+            cout << move.format(true) << " : " << flag << endl;
+            cout << "-----------------------------" << endl;
+
+            match.do_move(move);
+
+            depth++;
+
+        }
+
+    }
+
+
+    void test_does_move_touch_soft_pinned(cMatch &match, cMove &move){
+
+        bool flag = cEvaluator::does_move_touch_soft_pinned(match, move);
+        cout << "does_move_touch_soft_pinned: " << flag << endl;
+
+    }
+
+
+    void test_does_move_sac_for_supply(cMatch &match, cMove &move){
+
+        bool flag = cEvaluator::does_move_sac_for_supply(match, move);
+        cout << move.format(true) << " " << flag << endl;
+
+    }
 
--- util/test.hpp
+++ util/test.hpp
@@ -20,10 +20,20 @@
     
     void test_eval_piece_state(cMatch &match);
 
-    void test_is_hotspot(cMatch &match);
-
     void test_search_all_dirs_for_touching_pieces(cMatch &match);
 
     void test_eval_field_state(cMatch &match, const uint8_t src_x, const uint8_t src_y);
+
+    void test_gen2(cMatch &match);
+    
+    void test_does_move_escape_soft_pin(cMatch &match, const cMove &move);
+
+    void test_is_continue(cMatch &match, const uint8_t depth);
+
+    void test_is_continue_sequence(cMatch &match);
+
+    void test_does_move_touch_soft_pinned(cMatch &match, cMove &move);
+
+    void test_does_move_sac_for_supply(cMatch &match, cMove &move);
 
   #endif
